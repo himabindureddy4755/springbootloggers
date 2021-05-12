5c6b3ef44d99a0c377dfc03fb2a6be8b1e1be890
hp <bindureddy4755@gmail.com>
2021-05-12 15:20:41 +0530

    intitial commit

---
+++ SpringBootLoggingAppln/src/main/java/com/example/LogApp/SpringBootLoggingApplnApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingApplnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingApplnApplication.class, args);
+	}
+
+}
